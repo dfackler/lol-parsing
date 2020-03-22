4820d11241ab74ee46c6fabbb3dc223a32307db1
David Fackler <dfackler@gmail.com>
2020-03-22 10:32:12 -0700

    group_lists attempts to find best k, cluster, and save mapping file

--- group_lists.R
+++ group_lists.R
@@ -1,20 +1,23 @@
 # group_lists.R
 
 # read all files from a directory
-# assumption: all files are named using convention [attribute].txt and contain single column with identities
-# input: directory path (optional min_groups) (optional max_groups)
+# assumption: all files are named using convention [attribute].txt and contain single column with identities and no header
+# input: path to input directory, output file (full path)
 # output: grouping file
 
 library(dplyr)
 library(readr)
 library(stringr)
+library(cluster)
 
 # input_dir = "/Users/dfackler/Desktop/lol_training_data/Animals_with_Attributes2/prepped"
+# output_file = "/Users/dfackler/Desktop/lol_training_data/Animals_with_Attributes2/grouping_map.txt"
 ##############################################
 #### Read args and check files ####
 ##############################################
 args = commandArgs(trailingOnly=TRUE)
 input_dir = args[1]
+output_file = args[2]
 
 if(!dir.exists(input_dir)){
   print(paste0("Input path provided does not exist. Exiting.\nInput path: ", input_dir, "\n"))
@@ -33,7 +36,8 @@ if(length(files_to_read) == 0){
 lol <- list()
 unique_ids <- vector()
 for(i in 1:length(files_to_read)){
-  fl = read_csv(paste(input_dir, files_to_read[i], sep = "/"), col_names = FALSE) %>% pull(X1)
+  fl = read_csv(paste(input_dir, files_to_read[i], sep = "/"), 
+                col_names = FALSE, col_types = cols(X1 = col_character())) %>% pull(X1)
   unique_ids <- unique(c(unique_ids, fl))
   lol[[i]] <- fl
 }
@@ -45,30 +49,37 @@ for(i in 1:length(files_to_read)){
 # TODO: add sampling for when data gets big?
 # TODO: keep as sparse matrix for when data gets big?
 id_df <- data.frame(t(data.frame(lapply(lol, FUN = function(x){unique_ids %in% x}))))
-# id_df <- as_tibble(t(as_tibble(lapply(lol, FUN = function(x){unique_ids %in% x}), .name_repair = "universal")), .name_repair = "universal")
 rownames(id_df) <- str_replace(files_to_read, ".txt", "")
 colnames(id_df) <- unique_ids
+
+print(paste0("Total number of identities: ", length(unique_ids)))
+print(paste0("Total number of files: ", length(files_to_read)))
 
 ##############################################
 #### Set groups ####
 ##############################################
 # find good k value
 set.seed(123)
-# TODO: choose sequence based on number of files or from argument(s)?
-# k_vals <- c(seq(5, 10, 2), seq(11, 31, 5))
-k_vals <- 1:50
-wss <- vector()
-for (i in k_vals) {
-  wss[i] <- sum(kmeans(id_df, centers=i, nstart = 5)$withinss)
+# sillhouette method: https://medium.com/codesmart/r-series-k-means-clustering-silhouette-794774b46586
+silhouette_score <- function(k, df){
+  km <- pam(df, k = k) # more robust kmeans
+  ss <- silhouette(km$clustering, dist(df))
+  return(mean(ss[, 3]))
 }
-plot(1:length(wss), wss, type="b", xlab="Number of Clusters",
-     ylab="Within groups sum of squares")
-
-# TODO: base on elbow calculation or something else
-best_k_vals <- c(4, 7, 10, 15)
-best_k_guess <- 10
+# don't allow more than 100 iterations but try to get best k guess between 4 and num_obs/2
+seq_step <- max(2, floor((nrow(id_df)-4)/100)) 
+k_vals <- seq(4, floor(nrow(id_df)/2), seq_step) 
+avg_sil <- sapply(k_vals, silhouette_score, id_df)
+# plot(k_vals, type='b', avg_sil, xlab='Number of clusters', ylab='Average Silhouette Scores', frame=FALSE)
+best_k_guess <- k_vals[which(avg_sil == max(avg_sil))]
 
 # create dataframe of identities and groups of different values
+km <- pam(id_df, k = best_k_guess)
+clus_info <- data.frame(cluster = 1:best_k_guess, km$clusinfo, stringsAsFactors = FALSE)
+clus_info <- clus_info %>% arrange(desc(size))
+grouping_df <- data.frame(files = files_to_read,
+                          grouping = km$clustering,
+                          stringsAsFactors = FALSE)
 
 ##############################################
 #### Provide outputs ####
@@ -79,4 +90,12 @@ best_k_guess <- 10
 # save file of grouping mappings
 # save file of best grouping guess mapping
 # save kmeans plot (optional with argument?)
+write_csv(grouping_df, output_file)
+print(paste0("Grouping file written to: ", output_file))
+print(paste0("Best K Guess (using sillhouette method): ", best_k_guess))
+print("Grouping counts:")
+print(table(pull(grouping_df, grouping)))
+print("Cluster metrics:")
+print(clus_info)
+
 
